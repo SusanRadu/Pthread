1adabbca83be4f2f9a412a09f5250e65b964a517
SusanRadu <45876287+SusanRadu@users.noreply.github.com>
2020-01-16 16:07:50 +0200

    Update atomic.h

--- Mutex/atomic.h
+++ Mutex/atomic.h
@@ -1,3 +1,11 @@
+/*
+* - Library: atomic
+* - author: Radu Susan
+* - date: 08.01.2020
+* - Subject: Operating Systems
+* - University of Bucharest, Faculty of Mathematics and Computer Science
+*/
+
 #ifdef _ATMOIC_H_
 #define _ATMOIC_H_
 
