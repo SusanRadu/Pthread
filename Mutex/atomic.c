5a203a6094156952596dc9d380dcb9f49568f700
SusanRadu <45876287+SusanRadu@users.noreply.github.com>
2020-01-16 16:06:49 +0200

    Update atomic.c

--- Mutex/atomic.c
+++ Mutex/atomic.c
@@ -1,6 +1,13 @@
+/*
+* - Library: atomic
+* - author: Radu Susan
+* - date: 08.01.2020
+* - Subject: Operating Systems
+* - University of Bucharest, Faculty of Mathematics and Computer Science
+*/
 
 #include "atomic.h"
-
+//atomic function to check and lock the mutex
 int atomic_lock(atomic_int *x)
 {
  atomic_int exp,new;
